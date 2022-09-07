b9098c8e02c7ccd391e64c0368e460c0946281a8
gary003 <gary.johnson003@gmail.com>
2022-09-07 19:19:56 +0200

    set in place a basic transaction

--- src/servicesData/user/index.ts
+++ src/servicesData/user/index.ts
@@ -1,8 +1,32 @@
-import { Model } from "sequelize/types"
+import { Model, Transaction } from "sequelize/types"
 import User from "../models/user"
 import Wallet from "../models/wallet"
 import { cleanSequelizeResponse, getAttributesToSelect } from "../helpers/index"
 import { userAttributeToSelect, walletAttributeToSelect } from "./dto2"
+import { createConnectionSequelize } from "../dataSource/link"
+
+export const getUserInfoById = async (userId: string) => {
+  const userAttributesToSelect = await getAttributesToSelect(userAttributeToSelect)
+  const walletAttributesToSelect = await getAttributesToSelect(walletAttributeToSelect)
+
+  const result: Model | null = await User.findOne({
+    include: [
+      {
+        model: Wallet,
+        attributes: walletAttributesToSelect,
+      },
+    ],
+    attributes: userAttributesToSelect,
+    where: {
+      userId,
+    },
+    raw: true,
+  })
+
+  if (!result) throw new Error("User not found")
+
+  return await cleanSequelizeResponse([result])
+}
 
 export const getAllUsersDB = async () => {
   const userAttributesToSelect = await getAttributesToSelect(userAttributeToSelect)
@@ -20,4 +44,73 @@ export const getAllUsersDB = async () => {
   })
 
   return await cleanSequelizeResponse(allUsersInfo)
+}
+
+export const transfertMoney = async (currency: string, giverId: string, recipientId: string, amount: number) => {
+  const userAttributesToSelect = await getAttributesToSelect(userAttributeToSelect)
+  const walletAttributesToSelect = await getAttributesToSelect(walletAttributeToSelect)
+
+  const t = await createConnectionSequelize().transaction()
+
+  try {
+    // Then, we do some calls passing this transaction as an option:
+    const [giverUserInfo]: Object[] = await getUserInfoById(giverId)
+
+    // @ts-ignore
+    const giverNewBalance = Number(giverUserInfo[currency]) - amount
+
+    // console.log({ giverUserInfo, giverNewBalance })
+
+    const giverWalletToUpdate: Model | null = await Wallet.findOne({ where: { userId: giverId }, raw: true })
+
+    if (!giverWalletToUpdate) throw new Error("No giver wallet to update")
+
+    // @ts-ignore
+    // console.log({ giverUserInfo, giverNewBalance, giverWalletToUpdate })
+
+    // @ts-ignore
+    const updateWalletGiverResult = await Wallet.update({ [currency]: giverNewBalance }, { where: { walletId: giverWalletToUpdate.walletId }, transaction: t }).catch((err) => console.log(err))
+
+    console.log({ updateWalletGiverResult })
+
+    if (!updateWalletGiverResult || updateWalletGiverResult[0] === 0) {
+      throw new Error("Impossible to update the giver wallet info")
+    }
+
+    // Then, we do some calls passing this transaction as an option:
+    const [recipientUserInfo]: Object[] = await getUserInfoById(recipientId)
+
+    // @ts-ignore
+    const recipientNewBalance = Number(recipientUserInfo[currency]) + amount
+
+    console.log({ recipientUserInfo, recipientNewBalance })
+
+    const recipientWalletToUpdate: Model | null = await Wallet.findOne({ where: { userId: recipientId }, raw: true })
+
+    if (!recipientWalletToUpdate) throw new Error("No recipient wallet to update")
+
+    // @ts-ignore
+    console.log(recipientWalletToUpdate.walletId, { recipientNewBalance })
+
+    // @ts-ignore
+    const updateWalletRecipientResult = await Wallet.update({ [currency]: recipientNewBalance }, { where: { walletId: recipientWalletToUpdate.walletId }, transaction: t })
+
+    console.log({ updateWalletRecipientResult })
+
+    if (updateWalletRecipientResult[0] === 0) {
+      throw new Error("Impossible to update the recipient wallet info")
+    }
+
+    await t.commit()
+
+    return true
+  } catch (error) {
+    // If the execution reaches this line, an error was thrown.
+    // We rollback the transaction.
+    console.log({ error })
+
+    await t.rollback()
+
+    return false
+  }
 }
--- tests/unit/src/services/user/index.test.ts
+++ tests/unit/src/services/user/index.test.ts
@@ -1,6 +1,7 @@
-require("dotenv").config()
+// require("dotenv").config()
 import { describe, expect, test } from "@jest/globals"
 import { getAllUsers } from "../../../../../src/services/user"
+import { transfertMoney } from "../../../../../src/servicesData/user"
 
 describe("src > services > user", () => {
   it("should get all the users from user DB service", async () => {
@@ -10,5 +11,12 @@ describe("src > services > user", () => {
     expect(res).not.toBeNull()
     expect(res[0].userId).toBeDefined()
     expect(res[0].userId.length > 0).toBeTruthy()
+  })
+
+  it("should transfert money", async () => {
+    const res = await transfertMoney("softCurrency", "35269564-0234-11ed-b939-0242ac120002", "68965564-0234-11ed-b939-0242ac120002", 40)
+    console.log({ res })
+
+    expect(res).toBeTruthy()
   })
 })
